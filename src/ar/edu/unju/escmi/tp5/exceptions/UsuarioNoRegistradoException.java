ea2734013b1bf3bfa7996e64fd7cd91b144d9cb5
Martin Barconte <barcontemartin12@gmail.com>
2024-10-03 23:26:46 -0300

    CollectionUsuario, Usuario y Exceptions. BarconteMartin

--- src/ar/edu/unju/escmi/tp5/exceptions/UsuarioNoRegistradoException.java
+++ src/ar/edu/unju/escmi/tp5/exceptions/UsuarioNoRegistradoException.java
@@ -1,9 +1,5 @@
 package ar.edu.unju.escmi.tp5.exceptions;
 
-public class UsuarioNoRegistradoException extends Exception{
-	private static final long serialVersionUID = 1L;
+public class UsuarioNoRegistradoException {
 
-	public UsuarioNoRegistradoException(String mensaje) {
-        super(mensaje);
-    }
 }
