4857f673bf50e155635474587443d283d77eb182
wiber <wiber@DESKTOP-29TD6KD>
2024-10-04 00:47:07 -0300

    Bibliotecario, Alumno y Main. LaraAltamirano_parte4

--- src/ar/edu/unju/escmi/tp5/dominio/Alumno.java
+++ src/ar/edu/unju/escmi/tp5/dominio/Alumno.java
@@ -1,15 +1,17 @@
 package ar.edu.unju.escmi.tp5.dominio;
 
+
 public class Alumno extends Usuario {
 	 	private int curso;
 	    private int numeroLibreta;
 
-	    public Alumno(String id, String nombre, String apellido, String email, int curso, int numeroLibreta) {
+	    public Alumno(int id, String nombre, String apellido, String email, int curso, int numeroLibreta) {
 	        super(id, nombre, apellido, email);
 	        this.curso = curso;
 	        this.numeroLibreta = numeroLibreta;
 	    }
-	   
+	    
+	    
 	    public int getCurso() {
 			return curso;
 		}
@@ -25,10 +27,10 @@ public class Alumno extends Usuario {
 		public void setNumeroLibreta(int numeroLibreta) {
 			this.numeroLibreta = numeroLibreta;
 		}
-		
+
+
 		@Override
 	    public void mostrarDatos() {
-			System.out.println("\nID del usuario: "+id);
 	        System.out.println("Alumno: " + nombre + " " + apellido);
 	        System.out.println("Curso: " + curso);
 	        System.out.println("Número de libreta: " + numeroLibreta);
--- src/ar/edu/unju/escmi/tp5/main/Main.java
+++ src/ar/edu/unju/escmi/tp5/main/Main.java
@@ -1,21 +1,16 @@
 package ar.edu.unju.escmi.tp5.main;
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 import java.util.InputMismatchException;
-import java.util.List;
 import java.util.Scanner;
-
 import ar.edu.unju.escmi.tp5.collections.CollectionLibro;
-import ar.edu.unju.escmi.tp5.collections.CollectionPrestamo;
 import ar.edu.unju.escmi.tp5.dominio.*;
 import ar.edu.unju.escmi.tp5.collections.CollectionUsuario;
-import ar.edu.unju.escmi.tp5.exceptions.*;
-import ar.edu.unju.escmi.tp5.utils.FechaUtil;
+
 
 public class Main {
     public static void main(String[] args) {
     	  Scanner scanner = new Scanner(System.in);
-	        String opc;
+	        int opc;
+	        
 	        do {
 	        
 	            System.out.println("******** MENU ******** ");
@@ -26,37 +21,31 @@ public class Main {
 	            System.out.println("5. Listar libros");
 	            System.out.println("6. Salir");
 	            System.out.print("Seleccione una opción: ");
-	            opc = scanner.nextLine();  
+	            
+	            opc = scanner.nextInt();
+	            scanner.nextLine();  
+
 	            switch(opc) {
-	                case "1":
+	                case 1:
 	                	registrarLibro(scanner);
 	                    break;
-	                case "2":
+	                case 2:
 	                	registrarUsuario(scanner);
-	                	break;	
-	                case "3":
-	                	registrarPrestamo(scanner);
-	                	break;
-	                case "4":
-	                    registrarDevolucion(scanner);
-	                    break;
-	                case "5":
-	                	List<Libro> libros = CollectionLibro.libros;
-	                    if(libros.isEmpty()) System.out.println("\nLa lista de libros esta vacia");
-	                    else CollectionLibro.mostrarLibros();
-	                    break;
-	                case "6":
-	                    System.out.println("\nSALIENDO");
+	                break;	
+	                case 6:
+	                    System.out.println("SALIENDO DEL MENU");
 	                    break;
 	                default:
-	                    System.out.println("\nOPCION INVALIDA. Vuelva a intentarlo");
+	                    System.out.println("OPCION INVALIDA. Intentelo nuevamente");
 	            }
-	        } while(!opc.equals("6"));
+	        } while(opc != 6);
+
 	        scanner.close();
+	     
 	    }
 
     public static void registrarLibro(Scanner scanner) {
-        System.out.print("\nIngrese el ID del libro: ");
+        System.out.print("Ingrese el ID del libro: ");
         String id = scanner.nextLine();
         System.out.print("Ingrese el título del libro: ");
         String titulo = scanner.nextLine();
@@ -69,24 +58,22 @@ public class Main {
         CollectionLibro.agregarLibro(libro);
     }
     
-    public static void registrarUsuario(Scanner scanner) throws InputMismatchException {
+    public static void registrarUsuario(Scanner scanner) throws InputMismatchException
+    {
     	try {
-            System.out.println("\nRegistrar usuario:");
+            System.out.println("Registrar usuario:");
             System.out.println("1. Alumno");
             System.out.println("2. Bibliotecario");
-            System.out.print("Seleccione una opción: ");
-            String opcion = scanner.nextLine();
+            int opcion = scanner.nextInt();
+            scanner.nextLine(); 
 
-            switch(opcion) {
-            case "1":
-            	System.out.print("\nIngrese el ID del usuario: ");
-                String id1 = scanner.nextLine();
+            if (opcion == 1) {
                 System.out.print("Ingrese el nombre del alumno: ");
-                String nombreAlumno = scanner.nextLine();
+                String nombre = scanner.nextLine();
                 System.out.print("Ingrese el apellido del alumno: ");
-                String apellidoAlumno = scanner.nextLine();
+                String apellido = scanner.nextLine();
                 System.out.print("Ingrese el email del alumno: ");
-                String emailAlumno = scanner.nextLine();
+                String email = scanner.nextLine();
                 System.out.print("Ingrese el curso del alumno: ");
                 int curso = scanner.nextInt();
                 scanner.nextLine(); 
@@ -94,134 +81,31 @@ public class Main {
                 int numeroLibreta = scanner.nextInt();
                 scanner.nextLine(); 
                 
-                Alumno alumno = new Alumno(id1, nombreAlumno, apellidoAlumno, emailAlumno, curso, numeroLibreta);
+
+                Alumno alumno = new Alumno(1, nombre, apellido, email, curso, numeroLibreta);
                 CollectionUsuario.agregarUsuario(alumno);
-                
-                alumno.mostrarDatos();
-                
-                System.out.println("\nUsuario agregado correctamente.");           
-            break;
-            case "2":
-            	System.out.print("\nIngrese el ID del usuario: ");
-                String id2 = scanner.nextLine();
+            } else if (opcion == 2) {
                 System.out.print("Ingrese el nombre del bibliotecario: ");
-                String nombreBibliotecario = scanner.nextLine();
+                String nombre = scanner.nextLine();
                 System.out.print("Ingrese el apellido del bibliotecario: ");
-                String apellidoBibliotecario = scanner.nextLine();
+                String apellido = scanner.nextLine();
                 System.out.print("Ingrese el email del bibliotecario: ");
-                String emailBibliotecario = scanner.nextLine();
+                String email = scanner.nextLine();
                 System.out.print("Ingrese el legajo del bibliotecario: ");
                 int legajo = scanner.nextInt();
                 scanner.nextLine(); 
                 
-                Bibliotecario bibliotecario = new Bibliotecario(id2, nombreBibliotecario, apellidoBibliotecario, emailBibliotecario, legajo);
+                Bibliotecario bibliotecario = new Bibliotecario(1, nombre, apellido, email, legajo);
                 CollectionUsuario.agregarUsuario(bibliotecario);
-                
-                bibliotecario.mostrarDatos();
-                
-                System.out.println("\nUsuario agregado correctamente.");           
-            break;
-            default: System.out.println("\nError al seleccionar opcion");
-            }       
-        } catch (InputMismatchException e) {
-            System.out.println("\nError: valor invalido \nNo se agrego el usuario.");
-            scanner.nextLine();
-        }
-    }
-    
-    public static void registrarPrestamo(Scanner scanner) {
-    	
-    	List<Libro> libros = CollectionLibro.libros;
-    	for (Libro libro : libros) {
-    		if(libro.isEstado()) {
-    			libro.mostrarDatos();
-    		}
-    	}
-    	try {    		
-        	System.out.print("\nIngrese el ID del libro que se va a prestar: ");
-        	String idLibro = scanner.nextLine();
-        	Libro libroPrestado = CollectionLibro.buscarLibroPorCodigo(idLibro);
-        	if (libroPrestado == null) {
-                throw new LibroNoEncontradoException("El libro con ID " + idLibro + " no se encuentra.");
             }
-        
-        	if (libroPrestado.isEstado()==false){
-                throw new LibroNoDisponibleException("\nEl libro con ID " + idLibro + " no está disponible para prestar.");
-            }
-             
-           	
-	    	List<Usuario> usuarios = CollectionUsuario.usuarios;
-	    	for(Usuario usuario : usuarios) {
-	    		usuario.mostrarDatos();
-	    	}
-	    	System.out.print("\nIngrese el ID del usuario a quien se va a prestar: ");
-	    	String idUsuario = scanner.nextLine();    	
-	    	Usuario usuarioPrestado = CollectionUsuario.buscarUsuarioPorID(idUsuario);
-	    	
-	    	if (usuarioPrestado == null) {
-	            throw new UsuarioNoRegistradoException("\nEl usuario con ID " + idUsuario + " no está registrado.");
-	        }
-	    	
-			System.out.print("\nIngrese el ID del préstamo: ");
-			String idPrestamo = scanner.nextLine();
-	    	
-			
-			System.out.print("Ingrese la fecha del préstamo (YYYY-MM-DD): ");
-			String fechaPrestamo = scanner.nextLine();
-			LocalDate fecPrestamo;
-			
-	        try {
-	        	
-	        	fecPrestamo = FechaUtil.convertirStringLocalDate(fechaPrestamo);
-	        	
-	        } catch (DateTimeParseException e) {
-	        	
-	            System.out.println("\nError: Formato inválido. Debe ser YYYY-MM-DD");
-	            return; 
-	            
-	        }
-	    	
-			Prestamo prestamo = new Prestamo(idPrestamo, fecPrestamo, null, libroPrestado, usuarioPrestado);
-			CollectionPrestamo.agregarPrestamo(prestamo);
-			prestamo.mostrarDatos();
-			System.out.println("\nPrestamo realizado correctamente.");
-			libroPrestado.setEstado(false);
-	
-	        } catch (LibroNoEncontradoException | LibroNoDisponibleException | UsuarioNoRegistradoException e) {
-	            System.out.println("\nError: " + e.getMessage());
-	        } catch (Exception e) {
-	            System.out.println("\nOcurrió un error inesperado: " + e.getMessage());
-	        }
-    }
-    
-    public static void registrarDevolucion(Scanner scanner) {
-        List<Prestamo> prestamos = CollectionPrestamo.prestamos;
-        
-        prestamos.stream().filter(prestamo -> prestamo.getFechaDevolucion()==null).forEach(prestamo -> prestamo.mostrarDatos());
-        
-        try {
-            System.out.print("\nIngrese el ID del préstamo ha devolver: ");
-            String idPrestamo = scanner.nextLine();
-            
-            Prestamo prestamo = prestamos.stream().filter(p -> p.getId().equals(idPrestamo)).findFirst().orElse(null);
-            
-            if (prestamo == null) {
-                System.out.println("\nError: Préstamo no encontrado.");
-                return;
-            }
-            
-            System.out.print("Ingrese la fecha de devolución (YYYY-MM-DD): ");
-            String fechaDevolucion = scanner.nextLine();
-            
-            LocalDate fecha = FechaUtil.convertirStringLocalDate(fechaDevolucion);
-            
-            prestamo.registrarDevolucion(fecha);
-            System.out.println("\nDevolución registrada correctamente.");
-            
-        } catch (DateTimeParseException e) {
-            System.out.println("\nError: Formato de fecha inválido.");
+
+            System.out.println("Usuarios registrados:");
+            CollectionUsuario.mostrarUsuarios();
         } catch (Exception e) {
-            System.out.println("\nError inesperado: " + e.getMessage());
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
+  
+
+   
